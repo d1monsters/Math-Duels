50696fb6853d351f4c5e82c879048e7222452a9b
Matvey <matvey.shatalin.07@gmail.com>
2026-09-14 22:15:45 +0300

    properties

--- backend/MathDuelBackend/build.gradle.kts
+++ backend/MathDuelBackend/build.gradle.kts
@@ -1,17 +1,27 @@
 plugins {
-    id("java")
+    id("application")
+    id("org.springframework.boot") version "4.1.1"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "org.example.Main"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    runtimeOnly("org.postgresql:postgresql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
